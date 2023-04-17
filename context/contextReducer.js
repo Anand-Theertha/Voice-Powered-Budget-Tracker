99fe328792c1bbd9445347b7ad8e233485ab20c9
AnandTheertha <anandtheerthav3@gmail.com>
2023-04-17 18:48:54 +0530

    Initial Commit

---
+++ context/contextReducer.js
@@ -0,0 +1,16 @@
+const contextReducer = (state , action) => {
+    switch (action.type) {
+        case 'DELETE_TRANSACTION':
+            
+            break;
+
+            case 'ADD_TRANSACTION':
+            
+                break;
+
+        default:
+            break;
+    }
+}
+
+ export default contextReducer;
